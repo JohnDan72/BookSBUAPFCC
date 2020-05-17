a6489156a290953d2bfbf6be37cc1d08a37f5c3a
Juan Dan Garcia <mr._dany@hotmail.com>
2020-05-16 23:38:43 -0500

    Comienzo del módulo Libros.php (inicio de vista)

---
+++ app/views/libros.php
@@ -0,0 +1,183 @@
+<?php include "templates/header.php";?>
+<!--Aqui va todo el css adicional de cada vista-->
+<link rel="stylesheet" href="<?= $BASE_PATH;?>public/css/libros.css">
+<!--Aquí escribe el título de la página actual-->
+<title>Libros</title>
+<script src="<?= $BASE_PATH;?>public/js/indexJS.js"></script>
+<?php include "templates/navbar.php";?>
+
+
+<div class="parallax-container valign-wrapper">
+  <div class="parallax"><img src="https://pixabay.com/get/57e8d34a4c53aa14f6da8c7dda35367b1d36d8ed5456794e_1280.jpg"></div>
+  <div id="imgParallax2" class="row center grey-text text-lighten-4  grey darken-1">
+  	<h5>Busca un libro perfecto para tí</h5>
+  </div>
+</div>
+
+<div class="container">
+	<div class="row grey-text text-darken-3 center">
+		<h3>Nuestros Libros</h3>
+		<div class="subheader">¡Encuentra el libro que tanto deseas y cómpralo ahora mismo!</div>
+	</div>
+	<div class="row">
+		<div class="col s12">
+			<div class="row">
+				<div class="col s12 m6 l4">
+					<div class="card sticky-action">
+					    <div class="card-image waves-effect waves-block waves-light" style="max-height: 500px;">
+					      <img class="activator" src="../../public/img/imgLibros/img12.jpg">
+					    </div>
+					    <div class="card-action">
+					      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+					      <p><a href="#">This is a link</a></p>
+					    </div>
+					    <div class="card-reveal">
+					      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+					      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+					    </div>
+					</div>
+				</div>
+				<div class="col s12 m6 l4">
+					<div class="card sticky-action">
+					    <div class="card-image waves-effect waves-block waves-light" style="max-height: 500px;">
+					      <img class="activator" src="../../public/img/imgLibros/img2.jpg">
+					    </div>
+					    <div class="card-action">
+					      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+					      <p><a href="#">This is a link</a></p>
+					    </div>
+					    <div class="card-reveal">
+					      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+					      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+					    </div>
+					</div>
+				</div>
+				<div class="col s12 m6 l4">
+					<div class="card sticky-action">
+					    <div class="card-image waves-effect waves-block waves-light" style="max-height: 500px;">
+					      <img class="activator" src="../../public/img/imgLibros/img3.jpg">
+					    </div>
+					    <div class="card-action">
+					      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+					      <p><a href="#">This is a link</a></p>
+					    </div>
+					    <div class="card-reveal">
+					      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+					      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+					    </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col s12">
+			<div class="row">
+				<div class="col s12 m6 l4 ">
+					<div class="card sticky-action hoverable ">
+					    <div class="card-image waves-effect waves-block waves-light" style="max-height: 500px;">
+					      <img class="activator" src="../../public/img/imgLibros/img4.jpg">
+					    </div>
+					    <div class="card-action">
+					      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+					      <p><a href="#">This is a link</a></p>
+					    </div>
+					    <div class="card-reveal">
+					      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+					      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+					    </div>
+					</div>
+				</div>
+				<div class="col s12 m6 l4">
+					<div class="card sticky-action">
+					    <div class="card-image waves-effect waves-block waves-light" style="max-height: 500px;">
+					      <img class="activator" src="../../public/img/imgLibros/img16.jpg">
+					    </div>
+					    <div class="card-action">
+					      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+					      <p><a href="#">This is a link</a></p>
+					    </div>
+					    <div class="card-reveal">
+					      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+					      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+					    </div>
+					</div>
+				</div>
+				<div class="col s12 m6 l4">
+					<div class="card sticky-action">
+					    <div class="card-image waves-effect waves-block waves-light" style="max-height: 500px;">
+					      <img class="activator" src="../../public/img/imgLibros/img6.jpg">
+					    </div>
+					    <div class="card-action">
+					      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+					      <p><a href="#">This is a link</a></p>
+					    </div>
+					    <div class="card-reveal">
+					      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+					      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+					    </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col s12">
+			<div class="row">
+				<div class="col s12 m6 l4">
+					<div class="card sticky-action">
+					    <div class="card-image waves-effect waves-block waves-light" style="max-height: 500px;">
+					      <img class="activator" src="../../public/img/imgLibros/img7.jpg">
+					    </div>
+					    <div class="card-action">
+					      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+					      <p><a href="#">This is a link</a></p>
+					    </div>
+					    <div class="card-reveal">
+					      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+					      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+					    </div>
+					</div>
+				</div>
+				<div class="col s12 m6 l4">
+					<div class="card sticky-action">
+					    <div class="card-image waves-effect waves-block waves-light" style="max-height: 500px;">
+					      <img class="activator" src="../../public/img/imgLibros/img8.jpg">
+					    </div>
+					    <div class="card-action">
+					      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+					      <p><a href="#">This is a link</a></p>
+					    </div>
+					    <div class="card-reveal">
+					      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+					      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+					    </div>
+					</div>
+				</div>
+				<div class="col s12 m6 l4">
+					<div class="card sticky-action">
+					    <div class="card-image waves-effect waves-block waves-light" style="max-height: 500px;">
+					      <img class="activator" src="../../public/img/imgLibros/img23.jpg">
+					    </div>
+					    <div class="card-action">
+					      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+					      <p><a href="#">This is a link</a></p>
+					    </div>
+					    <div class="card-reveal">
+					      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+					      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+					    </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+<?php include "templates/footer.php";?>
+<!--Aquí va todo el javascript que quieras incluir-->
+<script type="text/javascript">
+	
+</script>
+<?php include "templates/end.php";?>
